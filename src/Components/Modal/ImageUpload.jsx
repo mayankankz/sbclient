b029f6f9c279d5fddb11dc3bea6f1d88f7d5834b
mayank <yoyomandown@gmail.com>
2024-07-28 14:28:47 +0530

    class

--- src/Components/Modal/ImageUpload.jsx
+++ src/Components/Modal/ImageUpload.jsx
@@ -1,9 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Form, FormGroup, Label, Input } from 'reactstrap';
-import axios from 'axios';
-import { apiUrl } from '../../utils/constant';
-import './imageModel.css'
-const ImageUploadModal = ({ isOpen, toggle,addElement }) => {
+import React, { useState, useEffect } from "react";
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+} from "reactstrap";
+import axios from "axios";
+import { apiUrl } from "../../utils/constant";
+import "./imageModel.css";
+import { DeleteOutline } from "@mui/icons-material";
+const ImageUploadModal = ({ isOpen, toggle, addElement }) => {
   const [images, setImages] = useState([]);
 
   const fetchImages = async () => {
@@ -11,7 +22,7 @@ const ImageUploadModal = ({ isOpen, toggle,addElement }) => {
       const response = await axios.get(`${apiUrl}/api/images`);
       setImages(response.data);
     } catch (error) {
-      console.error('Error fetching images:', error);
+      console.error("Error fetching images:", error);
     }
   };
 
@@ -21,49 +32,65 @@ const ImageUploadModal = ({ isOpen, toggle,addElement }) => {
 
   const handleImageUpload = async (event) => {
     const formData = new FormData();
-    formData.append('image', event.target.files[0]);
+    formData.append("image", event.target.files[0]);
 
     try {
       await axios.post(`${apiUrl}/api/upload`, formData, {
         headers: {
-          'Content-Type': 'multipart/form-data',
+          "Content-Type": "multipart/form-data",
         },
       });
       fetchImages(); // Refresh the image list after upload
     } catch (error) {
-      console.error('Error uploading image:', error);
+      console.error("Error uploading image:", error);
     }
   };
 
-  function handleClick(type,url){
-    addElement(type,url);
+  function handleClick(type, url) {
+    addElement(type, url);
     toggle();
   }
 
   return (
-    <Modal isOpen={isOpen} toggle={toggle} size='xl'>
+    <Modal isOpen={isOpen} toggle={toggle} size="xl">
       <ModalHeader toggle={toggle}>Upload Images</ModalHeader>
-      <ModalBody  style={{minHeight: '500px'}}>
-        <Form className='mb-5 w-50'>
+      <ModalBody style={{ minHeight: "500px" }}>
+        <Form className="mb-5 w-50">
           <FormGroup>
-            
-            <Input 
-              type="file" 
-              name="file" 
-              id="imageUpload" 
-              onChange={handleImageUpload} 
+            <Input
+              type="file"
+              name="file"
+              id="imageUpload"
+              onChange={handleImageUpload}
             />
           </FormGroup>
-          
         </Form>
         <div className="image-preview d-flex gap-4 flex-wrap mt-5 ">
-            {images.map((image, index) => (
-              <img onClick={()=> handleClick('image',image)} style={{border: '1px dotted black',padding: '10px'}} height={90} width={90} key={index} src={image} alt={`upload-${index}`} className="uploaded-image" />
-            ))}
-          </div>
+          {images.map((image, index) => (
+            <>
+            <div style={{width: 120, height: 120}}>
+            <img
+              onClick={() => handleClick("image", image)}
+              style={{ border: "1px dotted black", padding: "10px",width: "100%", height: "100%"}}
+             
+              key={index}
+              src={image.filePath}
+              alt={`upload-${index}`}
+              className="uploaded-image"
+            />
+            <div className="d-flex justify-content-between" style={{maxWidth: '120px'}}>
+            <p style={{fontSize: 10}}>{image.fileName.slice(0,15) + "..."}</p>
+            <DeleteOutline fontSize="25px" />
+            </div>
+            </div>
+            </>
+          ))}
+        </div>
       </ModalBody>
       <ModalFooter>
-        <Button color="secondary" onClick={toggle}>Close</Button>
+        <Button color="secondary" onClick={toggle}>
+          Close
+        </Button>
       </ModalFooter>
     </Modal>
   );
